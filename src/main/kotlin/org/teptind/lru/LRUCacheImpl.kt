2b41a29d69da7e1ab3e17dbcbe8611c599a21cad
teptind <teptind@gmail.com>
2021-10-31 17:41:24 +0300

    The implementation and basic tests have been added

--- src/main/kotlin/org/teptind/lru/LRUCacheImpl.kt
+++ src/main/kotlin/org/teptind/lru/LRUCacheImpl.kt
@@ -1,19 +1,75 @@
 package org.teptind.lru
 
-class LRUCacheImpl<K, V>(private val capacity: UInt) : LRUCache<K, V> {
+class LRUCacheImpl<K, V>(private val capacity: Int) : LRUCache<K, V> {
+    companion object {
+        const val THE_KEY_MUST_BE_IN_MAP_MSG = "The key must be in map"
+    }
+
+    init {
+        assert(capacity > 0) { "The capacity must be positive" }
+    }
 
     private val state: CacheState<K, V> = CacheState(null, null, mutableMapOf())
 
     override fun put(key: K, value: V) {
-        TODO("Not yet implemented")
+        if (state.nodeMap.containsKey(key)) {
+            cutOutNode(state.nodeMap[key]!!)
+        }
+
+        val node = Node(key, value)
+        state.nodeMap[key] = node
+        appendNode(node)
+
+        if (state.nodeMap.size > capacity) {
+            moveHead()
+        }
+
+        assert(state.nodeMap.containsKey(key)) { THE_KEY_MUST_BE_IN_MAP_MSG }
+        assert(state.nodeMap.size <= capacity) { "Size must be less than capacity" }
     }
 
     override fun get(key: K): V {
-        TODO("Not yet implemented")
+        assert(state.nodeMap.containsKey(key)) { THE_KEY_MUST_BE_IN_MAP_MSG }
+
+        val node = state.nodeMap[key]!!
+
+        updatePriority(node)
+
+        return node.value
     }
 
     override fun has(key: K): Boolean {
-        TODO("Not yet implemented")
+        return state.nodeMap.containsKey(key)
+    }
+
+    private fun updatePriority(node: Node<K, V>) {
+        cutOutNode(node)
+        appendNode(node)
+    }
+
+    private fun cutOutNode(node: Node<K, V>) {
+        node.prev?.run { next = node.next }
+        node.next?.run { prev = node.prev }
+    }
+
+    private fun appendNode(node: Node<K, V>) {
+        state.tail?.let {
+            it.next = node
+            node.prev = it
+            node.next = null
+        }
+
+        state.head = state.head ?: node
+        state.tail = node
+    }
+
+    private fun moveHead() {
+        assert(state.head != null) {"head must not be null"}
+
+        state.nodeMap.remove(state.head!!.key)
+
+        state.head = state.head?.next
+        state.head?.prev = null
     }
 
     data class Node<K, V>(val key: K, val value: V) {
@@ -22,8 +78,8 @@ class LRUCacheImpl<K, V>(private val capacity: UInt) : LRUCache<K, V> {
     }
 
     data class CacheState<K, V>(
-        var tail: Node<K, V>?,
         var head: Node<K, V>?,
+        var tail: Node<K, V>?,
 
         val nodeMap: MutableMap<K, Node<K, V>>
     )
--- src/test/kotlin/org/teptind/lru/LRUCacheTest.kt
+++ src/test/kotlin/org/teptind/lru/LRUCacheTest.kt
@@ -1,8 +1,56 @@
 package org.teptind.lru
 
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.AssertionError
 import kotlin.test.assertEquals
 
 class LRUCacheTest {
 
+    private fun fillCache(keyPrefix: String, keys: IntRange, cache: LRUCache<String, Int>) {
+        keys.forEach { cache.put("$keyPrefix$it", it) }
+    }
+
+    @Test
+    fun baseTest() {
+        val cache: LRUCache<String, Int> = LRUCacheImpl(10)
+
+        val keys = (1..8)
+        val keyPrefix = "key"
+        fillCache(keyPrefix, keys, cache)
+
+        keys.forEach {
+            assertEquals(true, cache.has("$keyPrefix$it"))
+            assertEquals(it, cache.get("$keyPrefix$it"))
+        }
+    }
+
+    @Test
+    fun capacityOverflowTest() {
+        val cache: LRUCache<String, Int> = LRUCacheImpl(5)
+
+        val keys = (1..8)
+        val displacedKeys = (1..3)
+        val leftKeys = keys.minus(displacedKeys)
+
+        val keyPrefix = "key"
+        fillCache(keyPrefix, keys, cache)
+
+        displacedKeys.forEach {
+            assertEquals(false, cache.has("$keyPrefix$it"))
+        }
+
+        leftKeys.forEach {
+            assertEquals(true, cache.has("$keyPrefix$it"))
+            assertEquals(it, cache.get("$keyPrefix$it"))
+        }
+    }
+
+    @Test
+    fun illegalCapacityTest() {
+        assertThrows<AssertionError> { LRUCacheImpl<Int, Int>(0) }
+            .also { assertEquals("The capacity must be positive", it.message) }
+    }
+
 }
+
